39627ab33f32b7012025ed65ccf4e008c3b79a7e
Mark Boychuk <boychuk.m.n@gmail.com>
2018-06-13 16:48:50 +0300

    added redis config

--- config/database.php
+++ config/database.php
@@ -26,5 +26,17 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
         ],
-    ]
+    ],
+    'redis' => [
+
+        'cluster' => env('REDIS_CLUSTER', false),
+
+        'default' => [
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DATABASE', 1),
+            'password' => env('REDIS_PASSWORD', null),
+        ],
+
+    ],
 ];
